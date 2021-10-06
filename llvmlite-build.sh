267b57e70c296bd3af22e261d48167b56677e08f
Todd A. Anderson <drtodd13@comcast.net>
2021-10-05 18:49:08 -0700

    Fix llvmlite build

--- llvmlite-build.sh
+++ llvmlite-build.sh
@@ -8,7 +8,7 @@ export CC=gcc-9
 export CXX=g++-9
 #find . -name "*.so" -exec rm {} \;
 rm -rf build
-config=../llvm-project-install/bin/llvm-config
+config=../llvm-project/llvm-project-install/bin/llvm-config
 LLVM_CONFIG=$config python setup.py clean
 EXTRA_LLVM_LIBS="-L /opt/intel/intelpython3/lib -fno-lto" LDFLAGS=-fPIC LLVM_CONFIG=$config python setup.py build --force
 EXTRA_LLVM_LIBS="-L /opt/intel/intelpython3/lib -fno-lto" LLVM_CONFIG=$config python setup.py install
