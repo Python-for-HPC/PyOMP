1548c4ba76ae559fdadb50d1d4414bf86e1ae7f1
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-04-25 06:42:46 -0700

    Update README and RTD conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,44 @@
 # Configuration file for the Sphinx documentation builder.
 
+import subprocess
+
 # -- Project information
 
-project = 'PyOMP'
-copyright = '2024, PyOMP developers'
-author = 'Giorgis Georgakoudis'
+project = "PyOMP"
+copyright = "2024, PyOMP developers"
+author = "Giorgis Georgakoudis"
 
-release = '0.1'
-version = '0.1.0'
+try:
+    release = (
+        subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        .strip()
+        .decode()
+    )
+except subprocess.CalledProcessError:
+    release = "latest"
+version = release
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
