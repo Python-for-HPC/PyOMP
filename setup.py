6815de3d887696b96956efafe8c315f23318ca0b
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-08-19 17:12:57 -0700

    Build OpenMP libs through setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ import sysconfig
 import subprocess
 import shutil
 import numpy as np
+import tarfile
+import urllib
+import sys
+import os
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from setuptools.command.build_clib import build_clib
 
+OPENMP_URL = "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/openmp-14.0.6.src.tar.xz"
+OPENMP_SHA256 = "4f731ff202add030d9d68d4c6daabd91d3aeed9812e6a5b4968815cfdff0eb1f"
 
 nrt_static = (
     "nrt_static",
@@ -68,54 +74,143 @@ class BuildStaticNRT(build_clib):
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir):
+    def __init__(self, name, *, sourcedir=None, url=None, sha256=None, cmake_args=[]):
         # Don't invoke the original build_ext for this special extension.
         super().__init__(name, sources=[])
+        if sourcedir and url:
+            raise ValueError(
+                "CMakeExtension should have either a sourcedir or a url, not both."
+            )
         self.sourcedir = sourcedir
+        self.url = url
+        self.sha256 = sha256
+        self.cmake_args = cmake_args
 
 
-class BuildIntrinsicsOpenMPPass(build_ext):
+class BuildCMakeExt(build_ext):
     def run(self):
         for ext in self.extensions:
             if isinstance(ext, CMakeExtension):
-                self.build_cmake(ext)
-                return
-        super().run()
+                self._prepare_source(ext)
+                self._build_cmake(ext)
+            else:
+                super().run()
 
-    def build_cmake(self, ext):
+    def _prepare_source(self, ext):
+        if ext.sourcedir:
+            return
+
+        tmp = Path(self.build_temp) / f"download-{ext.name}" / "src.tar.gz"
+        tmp.parent.mkdir(parents=True, exist_ok=True)
+        with urllib.request.urlopen(ext.url) as r:
+            with tmp.open("wb") as f:
+                f.write(r.read())
+
+        if ext.sha256:
+            import hashlib
+
+            sha256 = hashlib.sha256()
+            with tmp.open("rb") as f:
+                sha256.update(f.read())
+            if sha256.hexdigest() != ext.sha256:
+                raise ValueError(f"SHA256 mismatch for {ext.url}")
+
+        with tarfile.open(tmp) as tf:
+            # We assume the tarball contains a single directory with the source files.
+            ext.sourcedir = tmp.parent / tf.getnames()[0]
+            tf.extractall(tmp.parent)
+
+    def _build_cmake(self, ext: CMakeExtension):
         # Delete build directory if it exists to avoid errors with stale
         # CMakeCache.txt leftovers.
-        shutil.rmtree(self.build_temp, ignore_errors=True)
+        build_dir = Path(self.build_temp) / ext.name
+        shutil.rmtree(build_dir, ignore_errors=True)
+        build_dir.mkdir(parents=True, exist_ok=True)
 
         lib_dir = Path(
             self.get_finalized_command("build_py").get_package_dir("numba.openmp.libs")
         )
 
-        subprocess.run(
+        extra_cmake_args = self._env_toolchain_args()
+        # Set RPATH.
+        if sys.platform.startswith("linux"):
+            extra_cmake_args.append(r"-DCMAKE_INSTALL_RPATH=$ORIGIN")
+        elif sys.platform == "darwin":
+            extra_cmake_args.append(r"-DCMAKE_INSTALL_RPATH=@loader_path")
+
+        install_dir = Path(lib_dir) / ext.name
+        install_dir.mkdir(parents=True, exist_ok=True)
+        cfg = (
             [
                 "cmake",
                 "-S",
                 ext.sourcedir,
                 "-B",
-                self.build_temp,
+                build_dir,
                 "-DCMAKE_BUILD_TYPE=Release",
-                f"-DCMAKE_INSTALL_PREFIX={lib_dir}",
-            ],
+                f"-DCMAKE_INSTALL_PREFIX={install_dir}",
+            ]
+            + ext.cmake_args
+            + extra_cmake_args
+        )
+        subprocess.run(
+            cfg,
             check=True,
         )
 
-        subprocess.run(["cmake", "--build", self.build_temp, "-j"], check=True)
+        subprocess.run(["cmake", "--build", build_dir, "-j"], check=True)
         subprocess.run(
-            ["cmake", "--install", self.build_temp],
+            ["cmake", "--install", build_dir],
             check=True,
         )
+
+        # Remove symlinks in the lib directory to avoid issues with creating the
+        # wheel.
+        for file in lib_dir.rglob("*"):
+            if file.is_symlink():
+                file.unlink()
+
+    def _env_toolchain_args(self) -> list[str]:
+        args = []
+        # macOS archs/deployment target (cibuildwheel exposes these)
+        if archs := os.environ.get("ARCHS"):
+            args += [f"-DCMAKE_OSX_ARCHITECTURES={archs}"]
+        if minver := os.environ.get("MACOSX_DEPLOYMENT_TARGET"):
+            args += [f"-DCMAKE_OSX_DEPLOYMENT_TARGET={minver}"]
+
+        # Generic toolchain env
+        for var, cm in [
+            ("CC", "-DCMAKE_C_COMPILER="),
+            ("CXX", "-DCMAKE_CXX_COMPILER="),
+            ("AR", "-DCMAKE_AR="),
+            ("RANLIB", "-DCMAKE_RANLIB="),
+            ("NM", "-DCMAKE_NM="),
+        ]:
+            if os.environ.get(var):
+                args.append(cm + os.environ[var])
+
+        # Flags
+        if os.environ.get("CFLAGS"):
+            args.append(f"-DCMAKE_C_FLAGS={os.environ['CFLAGS']}")
+        if os.environ.get("CXXFLAGS"):
+            args.append(f"-DCMAKE_CXX_FLAGS={os.environ['CXXFLAGS']}")
+        if os.environ.get("LDFLAGS"):
+            args.append(f"-DCMAKE_EXE_LINKER_FLAGS={os.environ['LDFLAGS']}")
+        return args
 
 
 setup(
     libraries=[nrt_static],
-    ext_modules=[CMakeExtension("libIntrinsicsOpenMP", "numba/openmp/libs/pass")],
+    ext_modules=[
+        CMakeExtension("pass", sourcedir="numba/openmp/libs/pass"),
+        CMakeExtension(
+            "libomp",
+            url=OPENMP_URL,
+            sha256=OPENMP_SHA256,
+        ),
+    ],
     cmdclass={
         "build_clib": BuildStaticNRT,
-        "build_ext": BuildIntrinsicsOpenMPPass,
+        "build_ext": BuildCMakeExt,
     },
 )
