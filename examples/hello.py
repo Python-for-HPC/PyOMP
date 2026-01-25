b07b6624475f8171d59f08d20f44c88f6f69ebcc
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2026-01-25 06:02:57 -0800

    Revert changes to examples hello.py

--- examples/hello.py
+++ examples/hello.py
@@ -1,18 +1,10 @@
 from numba.openmp import njit
 from numba.openmp import openmp_context as openmp
-from numba.openmp import omp_get_num_threads, omp_get_thread_num, omp_set_num_threads
-import numpy as np
-
+from numba.openmp import omp_get_num_threads, omp_get_thread_num
 
 @njit
-def test_impl(nt):
-    omp_set_num_threads(nt)
-    a = np.zeros(4, dtype=np.int64)
-    with openmp("teams"):
-        with openmp("single"):
-            a[0] += 1
-        return a
+def hello():
+    with openmp("parallel num_threads(8)"):
+        print("hello thread", omp_get_thread_num(),"of", omp_get_num_threads())
 
-
-a = test_impl(4)
-print("a =", a)
+hello()
